209dc4d455badc102e2e5ed738e351d395b2ccfb
YADAV DEVANSHI <114089021+Devanshiyadav-2011@users.noreply.github.com>
2023-01-07 14:07:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+var canvas = new fabric.Canvas('myCanvas');
+var x= document.getElementById("myAudio");
+
+function new_image()
+{
+fabric.Image.fromURL('BirthdayImage.jpg',function(Img) {
+block_image_object = Img;
+
+});
+
+
+
+
+	
+
+
+}
+
+function playSound(){
+
+}
